31b18707bcd91fe9fbf4ff49bd2918d6cdbca56f
Shoeb Shaikh <90shoeb@gmail.com>
2023-07-16 12:52:54 +0800

    iframe

--- intro.py
+++ intro.py
@@ -13,7 +13,7 @@ file_path = Path('Introduction.pdf')
 with open(file_path,"rb") as f:
     base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 # st.write(base64_pdf)
-# pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1000" height="1000" type="application/pdf"></iframe>'
-pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="1000" height="1000" type="application/pdf">'
+pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1000" height="1000" type="application/pdf"></iframe>'
+# pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="1000" height="1000" type="application/pdf">'
     
 st.markdown(pdf_display, unsafe_allow_html=True)
